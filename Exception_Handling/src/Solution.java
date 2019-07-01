95c21fc96e805f618f4575abc27c151a229e605a
anushakolukulapalli <anusha.kolukulapalli@gmail.com>
2019-07-01 19:28:07 +0530

    First Commit

---
+++ Exception_Handling/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Solution {
+	public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            System.out.println("Hello");
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+}
